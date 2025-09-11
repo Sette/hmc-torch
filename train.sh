0323e1d1861cdec999271e5635823dd48eaaafd0
sette <brunosilvasette@gmail.com>
2025-09-11 09:14:11 -0300

    Update project

--- train.sh
+++ train.sh
@@ -9,7 +9,7 @@ export CUDA_LAUNCH_BLOCKING=1
 #           'eisen_FUN' 'expr_FUN' 'gasch1_FUN' 'gasch2_FUN' 'seq_FUN' 'spo_FUN')
 
 
-DATASETS="gasch1_GO gasch2_GO seq_GO spo_GO"
+DATASETS="cellcycle_GO derisi_GO eisen_GO expr_GO cellcycle_FUN derisi_FUN eisen_FUN expr_FUN gasch1_FUN gasch2_FUN seq_FUN spo_FUN"
 
 
 DATASET_PATH="./data"
