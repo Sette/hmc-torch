92eedef7c331cc3fbe7e1771a2f2cff8f8794886
sette <brunosilvasette@gmail.com>
2025-09-28 14:20:18 -0300

    update datasets

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ export CUDA_LAUNCH_BLOCKING=1
 
 # DATASETS="cellcycle_GO derisi_GO eisen_GO expr_GO gasch1_GO gasch2_GO seq_GO spo_GO cellcycle_FUN derisi_FUN eisen_FUN expr_FUN gasch1_FUN gasch2_FUN seq_FUN spo_FUN"
 
-DATASETS="cellcycle_GO derisi_GO eisen_GO expr_GO gasch1_GO gasch2_GO seq_GO spo_GO cellcycle_FUN derisi_FUN eisen_FUN expr_FUN gasch1_FUN gasch2_FUN spo_FUN"
+DATASETS="seq_GO spo_GO cellcycle_FUN derisi_FUN eisen_FUN expr_FUN gasch1_FUN gasch2_FUN spo_FUN"
 
 # DATASETS="seq_FUN"
 
