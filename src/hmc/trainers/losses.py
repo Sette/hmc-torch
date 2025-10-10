4dec85c5a135be36694331aeec6588676cadc244
sette <brunosilvasette@gmail.com>
2025-10-10 10:21:42 -0300

    refactor: rename loss calculation functions for clarity and maintainability

--- src/hmc/trainers/losses.py
+++ src/hmc/trainers/losses.py
@@ -1,9 +1,25 @@
 import torch
 
 
-
-def calculate_local_loss(output, target, criterion):
+def _calculate_local_loss(output, target, criterion):
 
     loss = criterion(output.double(), target)
+
+    return loss
+
+
+def calculate_local_loss(output, target, args):
+    """
+    Calculates the local loss using a specific criterion based on the current computation level.
+
+    Args:
+        output (torch.Tensor): The output tensor from the model.
+        target (torch.Tensor): The ground truth tensor.
+        args (Namespace): An object containing the current computation level and the criterions.
+    Returns:
+        torch.Tensor: The calculated loss.
+    """
+
+    loss = _calculate_local_loss(output, target, args.criterions[args.current_level])
 
     return loss
