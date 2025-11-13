6ce8472279bd39b1742aa62b39e6295544046818
sette <brunosilvasette@gmail.com>
2025-11-13 18:15:43 -0300

    Refactor job.py: add whitespace for readability and ensure newline at end of file

--- src/hmc/utils/train/job.py
+++ src/hmc/utils/train/job.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import time
 
+
 def create_job_id_name(prefix="job"):
     """
     Create a unique job ID using the current date and time.
@@ -16,12 +17,24 @@ def create_job_id_name(prefix="job"):
     return job_id
 
 
-
 def start_timer():
     return time.perf_counter()
+
 
 def end_timer(start):
     end = time.perf_counter()
     elapsed = end - start
     print(f"Tempo de treino: {elapsed:.2f} segundos")
     return elapsed
+
+
+def parse_str_flags(args):
+    """Convert certain string command-line arguments to boolean."""
+    args.best_theshold = args.best_theshold == "true"
+    args.use_sample = args.use_sample == "true"
+    args.hpo_by_level = args.hpo_by_level == "true"
+    args.save_torch_dataset = args.save_torch_dataset == "true"
+    args.warmup = args.warmup == "true"
+    args.focal_loss = args.focal_loss == "true"
+    args.hpo = args.hpo == "true"
+    return args
