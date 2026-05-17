60f069c90369e257fc144dd30169411a6249f920
sette <brunosilvasette@gmail.com>
2026-05-17 17:23:18 -0300

    feat(dataset): refactor dataset path imports and add label handling utilities

--- src/hmc/datasets/gofun/manager.py
+++ src/hmc/datasets/gofun/manager.py
@@ -9,7 +9,7 @@ import networkx as nx
 import numpy as np
 import torch
 
-from hmc.datasets.gofun import get_dataset_paths, to_skip
+from hmc.utils.datasets.paths import get_dataset_paths, to_skip
 from hmc.datasets.gofun.dataset_arff import HMCDatasetArff
 from hmc.utils.path.files import __load_json__
 
@@ -308,45 +308,3 @@ class HMCDatasetManager:  # pylint: disable=too-many-instance-attributes
             self.dataset_values["test"],
         )
 
-
-def initialize_dataset_experiments(
-    name: str,
-    device: str = "cpu",
-    dataset_path: str = "data/",
-    dataset_type="torch",
-    is_global: bool = False,
-) -> HMCDatasetManager:
-    """
-    Initialize and return an HMCDatasetManager for the specified dataset.
-
-    Parameters:
-    - name (str): Name of the dataset to load.
-    - output_path (str): Path to store output files.
-    - device (str, optional): Device to be used ('cpu' or 'cuda'). \
-        Default is 'cpu'.
-    - is_local (bool, optional): Whether to use local_classifier hierarchy. \
-        Default is False.
-    - is_global (bool, optional): Whether to use global hierarchy. \
-        Default is False.
-
-    Returns:
-    - HMCDatasetManager: Initialized dataset manager.
-    """
-    # Load dataset paths
-    datasets = get_dataset_paths(dataset_path=dataset_path)
-
-    # Validate if the dataset exists
-    if name not in datasets:
-        raise ValueError(
-            f"Dataset '{name}' not found in experiments datasets. \
-            Available datasets: {list(datasets.keys())}"
-        )
-
-    # Initialize dataset manager
-    kwargs = {
-        "dataset": datasets[name],
-        "dataset_type": dataset_type,
-        "device": device,
-        "is_global": is_global,
-    }
-    return HMCDatasetManager(**kwargs)
