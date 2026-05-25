3620bceb1af9baa0638525b1d8e6ccf24e98fcdf
sette <brunosilvasette@gmail.com>
2026-05-25 19:16:22 -0300

    feat(arxiv): add command-line options for ArXiv feature type and model name

--- run.sh
+++ run.sh
@@ -16,6 +16,8 @@ OUTPUT_PATH="./results"
 METHOD="local"
 SEED=0
 DATASET_TYPE="arff"
+ARXIV_FEATURE_TYPE="tfidf"
+ARXIV_MODEL_NAME="sentence-transformers/all-MiniLM-L6-v2"
 HPO="false"
 N_TRIALS=30
 JOB_ID="none" 
@@ -82,6 +84,8 @@ while [ "$#" -gt 0 ]; do
         --dataset_path) DATASET_PATH="$2"; shift ;;
         --seed) SEED="$2"; shift ;;
         --dataset_type) DATASET_TYPE="$2"; shift ;;
+        --arxiv_feature_type) ARXIV_FEATURE_TYPE="$2"; shift ;;
+        --arxiv_model_name) ARXIV_MODEL_NAME="$2"; shift ;;
         --batch_size) BATCH_SIZE="$2"; shift ;;
         --lr_values) LR_VALUES=($2); shift ;;
         --dropout_values) DROPOUT_VALUES=($2); shift ;;
@@ -117,6 +121,8 @@ cmd="python -m hmc.main \
                 --save_torch_dataset $SAVE_TORCH_DATASET \
                 --batch_size $BATCH_SIZE \
                 --dataset_type $DATASET_TYPE \
+                --arxiv_feature_type $ARXIV_FEATURE_TYPE \
+                --arxiv_model_name "$ARXIV_MODEL_NAME" \
                 --non_lin $NON_LIN \
                 --device $DEVICE \
                 --epochs $EPOCHS \
